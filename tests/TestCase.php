fed02936f4b578ad9f2720e50207805e9a57e039
Bulent Akgul <bulentakgul1980@gmail.com>
2023-02-10 14:00:36 +0300

    dependencies changed

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,18 +2,13 @@
 
 namespace Bakgul\LaravelTestsToReadme\Tests;
 
-use Bakgul\LaravelDumpServer\Concerns\HasDumper;
 use Tests\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use HasDumper;
-
     public function setUp(): void
     {
         parent::setUp();
-
-        $this->resetDumper();
     }
 
     public function tearDown(): void
